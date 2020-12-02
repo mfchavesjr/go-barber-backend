d9db7dbcbe3ff77bffcdef2d3bf225094e218d73
Maurício Chaves <feira.nova@gmail.com>
2020-12-02 16:22:41 -0300

    Testando criação de usuários

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,36 @@
+import AppError from '@shared/errors/AppError';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointmentService', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12345678',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12345678');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '12345678',
+    });
+
+    expect(createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '12345678',
+    })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,49 @@
+import { uuid } from 'uuidv4';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUsertDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '../../infra/typeorm/entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUserById = this.users.find(user => user.id === id);
+
+    return findUserById;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUserByEmail = this.users.find(user => user.email === email);
+
+    return findUserByEmail;
+  }
+
+  public async create({
+    name,
+    email,
+    password
+  }: ICreateUsertDTO): Promise<User> {
+    const user = new User();
+
+    user.id = uuid();
+    user.email = email;
+    user.password = password;
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+
+  }
+}
+
+export default UsersRepository;
